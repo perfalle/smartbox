a29464be963d067a1b3ff434cf5fc7a3c1f5475a
Max Amling <ugdrv@kit.edu>
2017-09-03 23:51:16 +0200

    build container images on VM with vagrant and ansible

---
+++ build/resources/build_flightcontrol_aci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./acbuild --debug begin docker://debian
+./acbuild --debug set-name flightcontrol
+./acbuild --debug run -- apt-get update
+./acbuild --debug run -- apt-get upgrade --yes
+./acbuild --debug run -- apt-get install --yes python3.4 python3-pip  python3-yaml  libyaml-dev $
+./acbuild --debug run -- pip3 install watchdog grpcio Jinja2
+./acbuild --debug run -- mkdir -p /homer/smartbox/com
+./acbuild --debug mount add com /home/smartbox/com
+./acbuild --debug run -- mkdir -p /dbus
+./acbuild --debug mount add dbussocketlocation /dbus
+./acbuild --debug run -- mkdir -p /unitfiles
+./acbuild --debug mount add unitfiles /unitfiles
+./acbuild --debug mount add root /root
+./acbuild --debug copy "./flightcontrol_startup.sh" "/startup.sh"
+./acbuild --debug run -- chmod +x /startup.sh
+./acbuild --debug set-exec -- /startup.sh
+./acbuild --debug write --overwrite flightcontrol.aci
+./acbuild --debug end
---
+++ build/resources/install_acbuild.sh
@@ -0,0 +1,5 @@
+cd ~
+git clone https://github.com/containers/build acbuild_repo
+cd acbuild_repo
+./build
+ mv ./bin/acbuild ~/acbuild
