bb93a54e0386b22382112f62d31476db611ba56c
Max Amling <ugdrv@kit.edu>
2018-01-20 06:15:50 +0100

    skip downloading/installing rkt if already downloaded

--- install_resources/install_rkt_debian.sh
+++ install_resources/install_rkt_debian.sh
@@ -2,6 +2,9 @@
 # Taken from https://coreos.com/rkt/docs/latest/distributions.html#deb-based
 
 set -e
+if [[ -f rkt_1.29.0-1_amd64.deb ]] ; then
+    exit
+fi
 gpg --recv-key 18AD5014C99EF7E3BA5F6CE950BDD3E0FC8A365E
 wget https://github.com/rkt/rkt/releases/download/v1.29.0/rkt_1.29.0-1_amd64.deb
 wget https://github.com/rkt/rkt/releases/download/v1.29.0/rkt_1.29.0-1_amd64.deb.asc
