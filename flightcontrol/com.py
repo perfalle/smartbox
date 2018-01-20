5f39080c3a2952518602cdf11a1edc67fb10a2d6
Max Amling <ugdrv@kit.edu>
2018-01-20 06:14:34 +0100

    added a not on how to resolve env vars in python

--- flightcontrol/com.py
+++ flightcontrol/com.py
@@ -5,7 +5,7 @@ import shutil
 import yaml
 import utils
 
-COM_ROOT_PATH = '$SMARTBOX_HOME/com'
+COM_ROOT_PATH = '$SMARTBOX_HOME/com' # cf. os.path.expandvars(path)
 
 WEBUI_PATH = os.path.join(COM_ROOT_PATH, 'webui')
 IMAGES_PATH = os.path.join(WEBUI_PATH, 'images')
