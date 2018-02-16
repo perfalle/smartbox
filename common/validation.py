2950e707d1f7a7f4ac45fb2cbfc20724c9d30fa7
Max Amling <ugdrv@kit.edu>
2018-02-16 03:45:40 +0100

    fixed paths

--- common/validation.py
+++ common/validation.py
@@ -1,10 +1,11 @@
 from jsonschema import Draft4Validator
-import yaml
+import yaml, os
 
-GLOBAL_CONFIG_SCHEMA_PATH = 'schemas/global_config_schema.yml'
-GLOBAL_STATUS_SCHEMA_PATH = 'schemas/global_status_schema.yml'
-SERVICE_CONFIG_SCHEMA_PATH = 'schemas/service_config_schema.yml'
-SERVICE_STATUS_SCHEMA_PATH = 'schemas/service_status_schema.yml'
+SCHEMAS_ROOT = '/root/common/schemas'
+GLOBAL_CONFIG_SCHEMA_PATH = os.path.join(SCHEMAS_ROOT, 'global_config_schema.yml')
+GLOBAL_STATUS_SCHEMA_PATH = os.path.join(SCHEMAS_ROOT, 'global_status_schema.yml')
+SERVICE_CONFIG_SCHEMA_PATH = os.path.join(SCHEMAS_ROOT, 'service_config_schema.yml')
+SERVICE_STATUS_SCHEMA_PATH = os.path.join(SCHEMAS_ROOT, 'service_status_schema.yml')
 
 
 def validate_global_config(global_config):
@@ -16,14 +17,14 @@ def validate_global_status(global_status):
 
 
 def validate_service_config(service_config):
-    return _validate_with_schema(global_config, SERVICE_CONFIG_SCHEMA_PATH)
+    return _validate_with_schema(service_config, SERVICE_CONFIG_SCHEMA_PATH)
 
 
 def validate_service_status(service_status):
-    return _validate_with_schema(global_config, SERVICE_STATUS_SCHEMA_PATH)
+    return _validate_with_schema(service_status, SERVICE_STATUS_SCHEMA_PATH)
 
 
 def _validate_with_schema(obj, schema_file):
     with open(schema_file, 'r') as fd:
-        validator = Draft4Validator(fd.safe_load(yaml_file.read()))
+        validator = Draft4Validator(yaml.safe_load(fd.read()))
     return list(map(lambda ve: ve.message, validator.iter_errors(obj)))
