297ed4c14f570520046d8e34b93400f9fff062df
Max Amling <ugdrv@kit.edu>
2018-02-16 03:48:20 +0100

    use common com

--- webui/mainapp/views.py
+++ webui/mainapp/views.py
@@ -7,29 +7,31 @@ import sys
 sys.path.append('..')
 from common import com, utils
 
+
 def show(request):
-    service_configs = com.read_all_service_configs()
-    status = com.read_all_service_statuses()
+    service_names = com.read_service_names()
     service_contexts = []
-    for service_name in service_configs:
-        service_config = service_configs[service_name]
-        service_status = status.get(service_name, {})
-        service_contexts.append(get_service_context(service_name,
-                                                    service_config,
-                                                    service_status))
+    for service_name in service_names:
+        service_config = com.read_service_config(service_name)
+        service_status = com.read_service_status(service_name)
+        service_contexts.append(
+            get_service_context(service_name, service_config, service_status))
     context = {'services': service_contexts}
     return render(request, 'show.html', context)
 
+
 def start(request):
     service_name = str(request.path)[len('/start/'):]
-    com.set_running_desired(service_name, True)
+    com.set_running(service_name, True)
     return redirect('/service/' + str(service_name))
+
 
 def stop(request):
     service_name = str(request.path)[len('/stop/'):]
-    com.set_running_desired(service_name, False)
+    com.set_running(service_name, False)
     #return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/show'))
     return redirect('/service/' + str(service_name))
+
 
 def add(request):
     if request.method == 'POST':
@@ -40,17 +42,16 @@ def add(request):
             if 'imagefile' in request.FILES:
                 file = request.FILES['imagefile']
                 handle_uploaded_file(file)
-                service_config['source'] = 'source_file'
                 service_config['source_file'] = file.name
             else:
-                service_config['source'] = 'source_url'
                 service_config['source_url'] = request.POST['url']
 
-            service_config['ports'] = None
-            service_config['volumes'] = None
-            service_config['backup_strategies'] = None
-            service_config['running_desired'] = False
-            com.add_service_config(request.POST['service_name'], service_config)
+            service_config['ports'] = {}
+            #service_config['volumes'] = {}
+            #service_config['backup_strategies'] = None
+            service_config['running'] = False
+            com.write_service_config(request.POST['service_name'],
+                                     service_config)
             return redirect('/show')
         else:
             print('not valid', request.POST, request.FILES)
@@ -58,26 +59,33 @@ def add(request):
         form = AddForm()
     return render(request, 'add.html', {'form': form})
 
+
 def handle_uploaded_file(f):
     utils.ensure_directory(com.IMAGES_PATH)
     with open(os.path.join(com.IMAGES_PATH, f.name), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
+
 def remove(request):
     service_name = str(request.path)[len('/remove/'):]
     com.remove_service_config(service_name)
     return redirect('/show')
 
+
 def get_service_context(service_name, service_config, service_status):
     service_context = dict()
     service_context['service_name'] = service_name
-    service_context['status'] = service_status.get('status', 'noimage') or 'noimage'
-    service_context['url_service_name'] = urllib.parse.quote(service_name) # -> urllib.parse.unquote
-    service_context['active_status'] = service_status.get('ActiveState', '-') or '-'
+    service_context['status'] = service_status.get('status',
+                                                   'noimage') or 'noimage'
+    service_context['url_service_name'] = urllib.parse.quote(
+        service_name)  # -> urllib.parse.unquote
+    service_context['active_status'] = service_status.get('ActiveState',
+                                                          '-') or '-'
     service_context['sub_status'] = service_status.get('SubState', '-') or '-'
-    utc_time = max(int(service_status.get('ActiveEnterTimestamp', 0) or 0),
-                   int(service_status.get('ActiveExitTimestamp', 0)) or 0)
+    utc_time = max(
+        int(service_status.get('ActiveEnterTimestamp', 0) or 0),
+        int(service_status.get('ActiveExitTimestamp', 0)) or 0)
     service_context['time'] = str(datetime.fromtimestamp(utc_time))
     if 'source_url' in service_config:
         service_context['source'] = service_config['source_url'] or str()
@@ -113,7 +121,8 @@ def service(request):
             for port_name in service_context['ports']:
                 direct_field_name = port_form.get_direct_field_name(port_name)
                 proxy_field_name = port_form.get_proxy_field_name(port_name)
-                hostname_field_name = port_form.get_hostname_field_name(port_name)
+                hostname_field_name = port_form.get_hostname_field_name(
+                    port_name)
                 try:
                     direct_port = int(request.POST[direct_field_name])
                 except (TypeError, ValueError):
@@ -123,9 +132,11 @@ def service(request):
                 except (TypeError, ValueError):
                     proxy_port = None
                 hostname = request.POST[hostname_field_name]
-                new_ports[port_name] = {'direct_port': direct_port,
-                                        'proxy_port': proxy_port,
-                                        'hostname': hostname}
+                new_ports[port_name] = {
+                    'direct_port': direct_port,
+                    'proxy_port': proxy_port,
+                    'hostname': hostname
+                }
             print(new_ports)
             #TODO: check for illegal port mappings
             com.set_ports(service_name, new_ports)
